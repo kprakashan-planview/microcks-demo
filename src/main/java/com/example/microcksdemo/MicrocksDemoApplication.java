aab21ca9d8dcbf2b9896a7e3dbd6887b6f5e1016
Kashyap Prakashan <kashyap.prakashan@WY1V4J5W3N.local>
2024-03-11 10:02:09 +0530

    Initial Commit

---
+++ src/main/java/com/example/microcksdemo/MicrocksDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.microcksdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocksDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocksDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microcksdemo/MicrocksDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microcksdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocksDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
